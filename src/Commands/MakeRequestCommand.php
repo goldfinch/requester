20ff4dcea54580e405d715eb539184b6c8c01da2
Art Karasev <as.karasyov@gmail.com>
2023-09-29 14:45:56 +1300

    add commands

---
+++ src/Commands/MakeRequestCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Goldfinch\Requester\Commands;
+
+use Goldfinch\Taz\Console\GeneratorCommand;
+use Goldfinch\Taz\Services\InputOutput;
+use Symfony\Component\Console\Command\Command;
+
+#[AsCommand(name: 'make:request')]
+class MakeRequestCommand extends GeneratorCommand
+{
+    protected static $defaultName = 'make:request';
+
+    protected $description = 'Create new reqeust';
+
+    protected $path = 'app/src/Requests';
+
+    protected $type = 'request';
+
+    protected $stub = './stubs/request.stub';
+
+    protected $prefix = 'Request';
+
+    protected function execute($input, $output): int
+    {
+        parent::execute($input, $output);
+
+        return Command::SUCCESS;
+    }
+}
